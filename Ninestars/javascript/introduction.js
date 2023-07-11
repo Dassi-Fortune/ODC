41a4ec6a95f84bf626565139a1ac3d1effe346c2
Dassi-fortune <dassifortune75@gmail.com>
2023-07-11 11:59:51 +0100

    initial commit

---
+++ Ninestars/javascript/introduction.js
@@ -0,0 +1,41 @@
+// This is a comment
+ /*
+ this is a multiple-linne comment
+ seconnd commen*/
+ // variable declararation
+ //var, let, const
+ // var and let declare variables that can be changed at any time while connst declare variables that cannnot be declared twice
+ 
+ //examples
+ var initialVariable;
+ var myVariable=10;
+ let myVariableName="hello";
+ const variable= 12;
+ console.log(typeof(initialVariable))
+ console.log(typeof(myVariable))
+ console.log(typeof(he))
+ console.log(typeof(myVariableName))
+ console.log(myVariable)
+ console.log(myVariableName)
+ console.log(variable)
+
+ /** operators in javascript */
+ // Addition, subtraction, multiplication, division and modulo
+ var a=2, b=10;
+ console.log(a+b)
+ console.log(a*b)
+ console.log(a/b)
+ console.log(a%b)
+ console.log(a-b)
+
+ var increment= 3, decrement =10;
+ let temp= 0
+ console.log(increment, decrement)
+ // increment++
+ temp= ++increment + 2
+ console.log(temp)
+ //decrement--
+ decrement = decrement - 1;
+ console.log(increment , decrement)
+
+ 
